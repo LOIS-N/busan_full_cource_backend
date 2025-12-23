be6899bd9a0036b0d021178323c34e5077d93647
suwon <suwon1863@gmail.com>
2025-12-23 10:36:00 +0900

    travle api 경로 수정

--- src/main/java/com/ssafy/gt/controller/TravelController.java
+++ src/main/java/com/ssafy/gt/controller/TravelController.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/travel")
+@RequestMapping("api/vi/travel")
 @RequiredArgsConstructor
 public class TravelController {
 
