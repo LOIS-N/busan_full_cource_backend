981f9e0c43a6d00b2a6eff2c71fef456e99d524c
hyeyeok <juliago00@naver.com>
2025-12-23 11:33:53 +0900

    악의 적인 url 수정

--- src/main/java/com/ssafy/gt/controller/TravelController.java
+++ src/main/java/com/ssafy/gt/controller/TravelController.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/vi/travel")
+@RequestMapping("/api/v1/travel")
 @RequiredArgsConstructor
 public class TravelController {
 
